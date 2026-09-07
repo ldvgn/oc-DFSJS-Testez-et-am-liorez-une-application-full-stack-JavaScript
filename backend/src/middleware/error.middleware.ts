ac2641abde03d26882e05d7beb2b037bcd8a66e4
Laury <l.devraigne@gmail.com>
2026-09-07 11:43:24 +0200

    refactor(backend): add request body validation middleware

--- backend/src/middleware/error.middleware.ts
+++ backend/src/middleware/error.middleware.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import { HttpError } from "../errors/http-error";
+import z, { ZodError } from "zod";
 
 export const errorHandler = (
   err: Error,
@@ -9,6 +10,10 @@ export const errorHandler = (
 ) => {
   if (err instanceof HttpError) {
     return res.status(err.status).json({ message: err.message });
+  }
+
+  if (err instanceof ZodError) {
+    return res.status(400).json({ errors: z.treeifyError(err) });
   }
 
   console.error(`[${req.method} ${req.originalUrl}]`, err);
